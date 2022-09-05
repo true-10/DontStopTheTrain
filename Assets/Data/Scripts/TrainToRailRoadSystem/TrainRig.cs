14ea38512fad2d2889b7ad6c92fa58ea22059af6
TRUE10NOTEBOOK\sergey <sergey.mozharovskiy@gmail.com>
2022-09-06 00:11:27 +0300

    Train To Rail Road System wip

--- Assets/Data/Scripts/TrainToRailRoadSystem/TrainRig.cs
+++ Assets/Data/Scripts/TrainToRailRoadSystem/TrainRig.cs
@@ -13,11 +13,14 @@ namespace DontStopTheTrain.TrainToRailRoadSystem
         [SerializeField] private TrainAxle frontAxle;
         [SerializeField] private TrainAxle rearAxle;
         [SerializeField] private float speed = 10f;
+        [SerializeField] private float length = 10f;
+        [SerializeField] private float scale =  7f;
+        [SerializeField] private float offset =  15f;
       //  [SerializeField] private Vector2 YAngleMinMax = new Vector2(-10f, 10f);
 
         private Transform rigTransform;
 
-
+        private float distanceTraveled = 0;
         private void Awake()
         {
             rigTransform = GetComponent<Transform>();
@@ -32,23 +35,41 @@ namespace DontStopTheTrain.TrainToRailRoadSystem
         // Update is called once per frame
         void Update()
         {
+            distanceTraveled += speed * Time.deltaTime;
+            SetAxlesToPathAtDistance(distanceTraveled);
+            UpdateRig(distanceTraveled);
 
         }
 
+        private void UpdateRig(float distance)
+        {
+            var direction = frontAxle.CachedTransform.position - rearAxle.CachedTransform.position;
+            var normalizedDirection = direction.normalized;
+            var newRigPos = rearAxle.CachedTransform.position + normalizedDirection * length / 2f;
+            //newRigPos += normalizedDirection * offset;
+            rigTransform.position = newRigPos;
+
+            var newRigRot = Quaternion.LookRotation(normalizedDirection);
+
+            rigTransform.rotation = newRigRot;
+        }
 
         private void SetAxlesToPathAtDistance(float distance)
         {
-
-            var axleDistance = distance + frontAxle.CachedTransform.localPosition.z;
+            var halfLength = length / scale / 2f;
+            var scaledOffset = offset / scale / 2f;
+            var axleDistance = distance + halfLength + scaledOffset;
             SetAxleToPathAtDistance(frontAxle, axleDistance);
-            axleDistance = distance + rearAxle.CachedTransform.localPosition.z;
+            axleDistance = distance - halfLength + scaledOffset;
             SetAxleToPathAtDistance(rearAxle, axleDistance);
         }
 
         private void SetAxleToPathAtDistance(TrainAxle axle, float distance) 
         {
+            Vector3 angleVector = Vector3.forward * 90f;
             axle.CachedTransform.position = pathCreator.path.GetPointAtDistance(distance);
             axle.CachedTransform.rotation = pathCreator.path.GetRotationAtDistance(distance);
+            axle.CachedTransform.Rotate(angleVector);
 
         }
     }
