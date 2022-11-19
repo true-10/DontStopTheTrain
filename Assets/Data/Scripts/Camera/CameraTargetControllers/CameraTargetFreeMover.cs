8e875f5c49fb89e1a8c6cc7107c984d4ea58a0f4
true10 <sergey.mozharovskiy@gmail.com>
2022-11-19 11:18:41 +0300

    wip

--- Assets/Data/Scripts/Camera/CameraTargetControllers/CameraTargetFreeMover.cs
+++ Assets/Data/Scripts/Camera/CameraTargetControllers/CameraTargetFreeMover.cs
@@ -65,9 +65,31 @@ namespace DontStopTheTrain.Gameplay
            // Debug.Log($"{name} hValue = {hValue} vValue = {vValue}");
 
             var direction = new Vector3 (hValue, 0f, vValue);
+            direction = ClampDirection(direction);
             Move(direction);
         }
 
+        public Vector3 ClampDirection(Vector3 direction)
+        {
+            var dotProduct = Vector3.Dot(direction, Vector3.forward);
+             Debug.Log($"CameraTargetFreeMover: dotProduct F = {dotProduct}");
+            if (Mathf.Abs(dotProduct) > 0.8f)
+            {
+                direction = Vector3.forward;
+                direction *= Mathf.Sign(dotProduct);
+                return direction;
+            }
+
+            Debug.Log($"CameraTargetFreeMover: dotProduct R = {dotProduct}");
+            dotProduct = Vector3.Dot(direction, Vector3.right);
+            if (Mathf.Abs(dotProduct) > 0.8f)
+            {
+                direction = Vector3.right;
+                direction *= Mathf.Sign(dotProduct);
+                return direction;
+            }
+            return direction;
+        }
         public void Move(Vector3 direction)
         {
             var position = cachedTransform.position;
