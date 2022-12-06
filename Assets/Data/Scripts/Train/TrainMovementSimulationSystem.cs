1ca66dd6d7b6d4955b8cf9d88425c37d33ade6a1
TRUE10NOTEBOOK\sergey <sergey.mozharovskiy@gmail.com>
2022-12-06 21:23:13 +0300

    wip

--- Assets/Data/Scripts/Train/TrainMovementSimulationSystem.cs
+++ Assets/Data/Scripts/Train/TrainMovementSimulationSystem.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Zenject;
+using True10.LevelScrollSystem;
 
 namespace DontStopTheTrain.Train
 {
@@ -10,13 +12,50 @@ namespace DontStopTheTrain.Train
 
     }
 
+    public class WagonRig
+    {
+        private Transform transform;
+        private LocalJump localJump;
+        private ShakeRotation shakeRotation;
+        private PunchRotation punchRotation;
+        private GoToZero goToZero;
+
+        public WagonRig(Transform transform, 
+            LocalJumpData localJumpData,
+            ShakeRotationData shakeRotationData,
+            PunchRotationData punchRotationData,
+            GoToZeroData goToZeroData
+            )
+        {
+            this.transform = transform;
+
+            localJump = new LocalJump(localJumpData);
+            shakeRotation = new ShakeRotation(shakeRotationData);
+            punchRotation = new PunchRotation(punchRotationData);
+            goToZero = new GoToZero(goToZeroData);
+        }
+
+        public void SimulateMovement(float multiplayer)
+        {
+            shakeRotation.Do(transform, multiplayer);
+            punchRotation.Do(transform, multiplayer);
+            localJump.Do(transform, multiplayer);
+            goToZero.Do(transform, multiplayer);
+        }
+    }
+
     public class TrainMovementSimulationSystem : MonoBehaviour
     {
-        [SerializeField] private LocalJump localJump = new LocalJump();
-        [SerializeField] private ShakeRotation shakeRotation = new ShakeRotation();
-        [SerializeField] private PunchRotation punchRotation = new PunchRotation();
-        [SerializeField] private GoToZero goToZero = new GoToZero();
-        [SerializeField] List<Transform> wagonRigs;
+        [Inject]
+        private ILevelScrollSpeedController levelScrollSpeedController;
+
+        [SerializeField] private LocalJumpData localJumpData;
+        [SerializeField] private ShakeRotationData shakeRotationData;
+        [SerializeField] private PunchRotationData punchRotationData;
+        [SerializeField] private GoToZeroData goToZeroData;
+        [SerializeField] private List<Transform> wagonRigsTransform;
+        
+        private List<WagonRig> wagonRigs = new();
 
 
         /*[SerializeField] private float xRotationLimit = 2f;
@@ -28,208 +67,291 @@ namespace DontStopTheTrain.Train
 
         [SerializeField] private float zDuration = 1f;
         */
-        void Start()
+        void Start() 
         {
-            Simulation();
+            Init();
         }
 
+
+        void Init()
+        {
+            for (int i = 0; i < wagonRigsTransform.Count; i++)
+            {
+                var t = wagonRigsTransform[i];
+                var wagonRig = new WagonRig(t, localJumpData, shakeRotationData, punchRotationData, goToZeroData);
+                wagonRigs.Add(wagonRig);
+            }
+        }
+
+        private void Update()
+        {
+           Simulation();
+        }
 
         void Simulation()
         {
-            foreach (Transform t in wagonRigs)
+            foreach (var wr in wagonRigs)
             {
-                SimulateMovement(t);
+                wr.SimulateMovement( levelScrollSpeedController.Multilplayer);
             }
         }
+    }
 
-        void SimulateMovement(Transform target)
-        {
-            shakeRotation.Do(target);
-            punchRotation.Do(target);
-            localJump.Do(target);
-            goToZero.Do(target);
-            //Rotation(target);
-        }
-
-
-
-
-        [System.Serializable]
-        public abstract class AbstractTransformModifier
-        {
-            public bool isEnable = false;
-            public LoopType loopType = LoopType.Restart;
-
-            public void Do(Transform target)
-            {
-                if (isEnable == false)
-                {
-                    return;
-                }
-                Init(target);
-                Modify(target);
-            }
-
-            protected virtual void Modify(Transform target)
-            {
-
-            }
-
-            protected virtual void Init(Transform target)
-            {
-
-            }
-
-        }
-
-        [System.Serializable]
-        public class ShakeRotation : AbstractTransformModifier
-        {
-
-            [SerializeField] private float shakeStrength = 2f;
-            [SerializeField] private float shakeDuration = 1f;
-            [SerializeField] private int shakeVibrato = 3;
-            [SerializeField] private float shakeRandomness = 20f;
-            [SerializeField] private bool shakeFadeOut = true;
-
-
-            protected override void Modify(Transform target)
-            {
-
-                target.DOShakeRotation(shakeDuration, shakeStrength, shakeVibrato, shakeRandomness, shakeFadeOut)
-                    .SetLoops(-1, loopType);
-            }
-
-            protected override void Init(Transform target)
-            {
-                shakeDuration *= (1f + Random.Range(-0.2f, 0.2f));
-                shakeRandomness *= (1f + Random.Range(-0.2f, 0.2f));
-            }
-        }
-
-        [System.Serializable]
-        public class PunchRotation : AbstractTransformModifier
-        {
-
-            [SerializeField] private Vector3 punchVector = Vector3.up;
-            [SerializeField] private float duration = 1f;
-            [SerializeField] private int vibrato = 3;
-            [SerializeField] private float elasticity = 1f;
-            [SerializeField] private bool shakeFadeOut = true;
-
-            protected override void Modify(Transform target)
-            {
-                target.DOPunchRotation(punchVector, duration, vibrato, elasticity)
-                    .SetLoops(-1, loopType);
-            }
-
-            protected override void Init(Transform target)
-            {
-
-                punchVector *= (1f + Random.Range(-0.2f, 0.2f));
-                duration *= (1f + Random.Range(-0.2f, 0.2f));
-            }
-        }
-
-        [System.Serializable]
-        public class LocalJump : AbstractTransformModifier
+    [System.Serializable]
+    public class AbstractTransformModifierData
     {
-            public bool snapping = true;
-            public float duration = 1f;
-            public float jumpPower = 0.5f;
-            public int numJumps = 3;
+        public bool IsEnable = false;
+        public LoopType LoopType = LoopType.Restart;
+        public float Duration = 1f;
+    }
 
-            protected override void Modify(Transform target)
-            {
-                target.DOLocalJump(Vector3.zero, jumpPower, numJumps, duration, snapping)
-                    .SetLoops(-1, loopType);
-            }
+    [System.Serializable]
+    public abstract class AbstractTransformModifier
+    {
+        protected bool isEnable = false;
+        protected LoopType loopType = LoopType.Restart;
+        protected float duration = 1f;
 
-            protected override void Init(Transform target)
-            {
-                jumpPower *= (1f + Random.Range(-0.2f, 0.2f));
-                duration *= (1f + Random.Range(-0.2f, 0.2f));
-            }
-        }
+        protected Tween tween = null;
+        protected float multiplayer;
 
+        protected abstract void Modify(Transform target);
+        protected abstract void Init(Transform target);
 
-
-        [System.Serializable]
-        public class GoToZero : AbstractTransformModifier
+        public AbstractTransformModifier(AbstractTransformModifierData data)
         {
-            public bool snapping = true;
-            public float duration = 1f;
-
-            protected override void Modify(Transform target)
-            {
-                target.DOLocalMove(Vector3.zero, duration, snapping )
-                    .SetLoops(-1, loopType);
-            }
-
-            protected override void Init(Transform target)
-            {
-
-            }
+            isEnable = data.IsEnable;
+            loopType = data.LoopType;
+            duration = data.Duration;
         }
 
-        /*void Rotation(Transform target)
+        public void Do(Transform target, float multiplayer)
+        {
+            if (isEnable == false)
+            {
+                return;
+            }
+            if (tween != null)
+            {
+                return;
+            }
+            this.multiplayer = multiplayer;
+            Init(target);
+            Modify(target);
+        }
+
+
+        protected void OnTweenComplete()
+        {
+            tween.Kill();
+            tween = null;
+        }
+    }
+
+    [System.Serializable]
+    public class ShakeRotationData : AbstractTransformModifierData
+    {
+        public float Strength = 2f;
+        public int Vibrato = 3;
+        public float Randomness = 20f;
+        public bool FadeOut = true;
+    }
+
+    [System.Serializable]
+    public class ShakeRotation : AbstractTransformModifier
+    {
+        private float shakeStrength = 2f;
+        private float shakeDuration = 1f;
+        private int shakeVibrato = 3;
+        private float shakeRandomness = 20f;
+        private bool shakeFadeOut = true;
+
+        public ShakeRotation(ShakeRotationData data) : base(data)
+    {
+            shakeStrength = data.Strength;
+            shakeVibrato = data.Vibrato;
+            shakeRandomness = data.Randomness;
+            shakeFadeOut = data.FadeOut;
+        }
+
+        protected override void Modify(Transform target)
+        {
+            tween = target.DOShakeRotation(shakeDuration, shakeStrength * multiplayer, shakeVibrato, shakeRandomness, shakeFadeOut)
+               //.SetLoops(-1, loopType);
+               .OnComplete(OnTweenComplete);
+        }
+
+        protected override void Init(Transform target)
+        {
+            shakeDuration *= (1f + Random.Range(-0.2f, 0.2f));
+            shakeRandomness *= (1f + Random.Range(-0.2f, 0.2f));
+        }
+    }
+
+    [System.Serializable]
+    public class PunchRotationData : AbstractTransformModifierData
+    {
+        public Vector3 PunchVector = Vector3.up;
+        public int Vibrato = 3;
+        public float Elasticity = 1f;
+        public bool ShakeFadeOut = true;
+    }
+
+    [System.Serializable]
+    public class PunchRotation : AbstractTransformModifier
+    {
+        private Vector3 punchVector = Vector3.up;
+        private int vibrato = 3;
+        private float elasticity = 1f;
+        private bool shakeFadeOut = true;
+
+        public PunchRotation(PunchRotationData data) : base(data)
+    {
+            punchVector = data.PunchVector;
+            vibrato = data.Vibrato;
+            elasticity = data.Elasticity;
+            shakeFadeOut = data.ShakeFadeOut;
+        }
+
+        protected override void Modify(Transform target)
+        {
+            tween = target.DOPunchRotation(punchVector * multiplayer, duration, vibrato, elasticity)
+               // .SetLoops(-1, loopType);
+               .OnComplete( OnTweenComplete );
+        }
+
+        protected override void Init(Transform target)
+        {
+            punchVector *= (1f + Random.Range(-0.2f, 0.2f));
+            duration *= (1f + Random.Range(-0.2f, 0.2f));
+        }
+    }
+
+    [System.Serializable]
+    public class LocalJumpData : AbstractTransformModifierData
+    {
+        public bool Snapping = true;
+        public float JumpPower = 0.5f;
+        public int NumJumps = 3;
+    }
+
+    [System.Serializable]
+    public class LocalJump : AbstractTransformModifier
+    {
+        private bool snapping = true;
+        private float jumpPower = 0.5f;
+        private int numJumps = 3;
+
+        public LocalJump(LocalJumpData data) : base(data)
+        {
+            snapping = data.Snapping;
+            jumpPower = data.JumpPower;
+            numJumps = data.NumJumps;
+        }
+
+        protected override void Modify(Transform target)
+        {
+            tween = target.DOLocalJump(Vector3.zero, jumpPower * multiplayer, numJumps, duration, snapping)
+             //   .SetLoops(-1, loopType);
+               .OnComplete(OnTweenComplete);
+        }
+
+        protected override void Init(Transform target)
+        {
+            jumpPower *= (1f + Random.Range(-0.2f, 0.2f));
+            duration *= (1f + Random.Range(-0.2f, 0.2f));
+        }
+    }
+
+
+    [System.Serializable]
+    public class GoToZeroData : AbstractTransformModifierData
+    {
+        public bool Snapping = true;
+    }
+
+    [System.Serializable]
+    public class GoToZero : AbstractTransformModifier
+    {
+        private bool snapping = true;
+
+        public GoToZero(GoToZeroData data) : base(data)
+        {
+            snapping = data.Snapping;
+        }
+        protected override void Modify(Transform target)
+        {
+            tween = target.DOLocalMove(Vector3.zero, duration, snapping)
+                //.SetLoops(-1, loopType);
+               .OnComplete(OnTweenComplete);
+        }
+
+        protected override void Init(Transform target)
+        {
+
+        }
+    }
+}
+
+
+
+
+/*void Rotation(Transform target)
 {
-   //xAngle = 
-   //     DOTween.To(() => xAngle, x => xAngle, xRotationLimit, xDuration);
-   XRotation(target);
-   YRotation(target);
-   ZRotation(target);
+//xAngle = 
+//     DOTween.To(() => xAngle, x => xAngle, xRotationLimit, xDuration);
+XRotation(target);
+YRotation(target);
+ZRotation(target);
 }
 
 
 void XRotation(Transform target)
 {
-   InitRandomXRotation(target);
-   var endRotation = Vector3.zero;
-   endRotation.x = xRotationLimit;
-   target.DOLocalRotate(endRotation, xDuration)
-       .SetLoops(-1, LoopType.Yoyo);
+InitRandomXRotation(target);
+var endRotation = Vector3.zero;
+endRotation.x = xRotationLimit;
+target.DOLocalRotate(endRotation, xDuration)
+   .SetLoops(-1, LoopType.Yoyo);
 }
 
 void YRotation(Transform target)
 {
-   InitRandomZRotation(target);
-   var endRotation = Vector3.zero;
-   endRotation.y = yRotationLimit;
-   // target.DOLocalRotate(endRotation, yDuration)
-   // .SetLoops(-1, LoopType.Yoyo);
-   target.DOShakeRotation(yDuration);
+InitRandomZRotation(target);
+var endRotation = Vector3.zero;
+endRotation.y = yRotationLimit;
+// target.DOLocalRotate(endRotation, yDuration)
+// .SetLoops(-1, LoopType.Yoyo);
+target.DOShakeRotation(yDuration);
 }
 
 void ZRotation(Transform target)
 {
-   InitRandomZRotation(target);
-   var endRotation = Vector3.zero;
-   endRotation.z = zRotationLimit;
-   target.DOLocalRotate(endRotation, zDuration)
-       .SetLoops(-1, LoopType.Yoyo);
+InitRandomZRotation(target);
+var endRotation = Vector3.zero;
+endRotation.z = zRotationLimit;
+target.DOLocalRotate(endRotation, zDuration)
+   .SetLoops(-1, LoopType.Yoyo);
 }
 
 void InitRandomXRotation(Transform target)
 {
-   var endRotation = Vector3.zero;
-   endRotation.x = Random.Range(-xRotationLimit, xRotationLimit);
-   target.DORotate(endRotation, 0);
+var endRotation = Vector3.zero;
+endRotation.x = Random.Range(-xRotationLimit, xRotationLimit);
+target.DORotate(endRotation, 0);
 }
 
 void InitRandomYRotation(Transform target)
 {
-   var endRotation = Vector3.zero;
-   endRotation.y = Random.Range(-yRotationLimit, yRotationLimit);
-   target.DORotate(endRotation, 0);
+var endRotation = Vector3.zero;
+endRotation.y = Random.Range(-yRotationLimit, yRotationLimit);
+target.DORotate(endRotation, 0);
 }
 
 void InitRandomZRotation(Transform target)
 {
-   var endRotation = Vector3.zero;
-   endRotation.z = Random.Range(-zRotationLimit, zRotationLimit);
-   target.DORotate(endRotation, 0);
+var endRotation = Vector3.zero;
+endRotation.z = Random.Range(-zRotationLimit, zRotationLimit);
+target.DORotate(endRotation, 0);
 }*/
-    }
 
-}
